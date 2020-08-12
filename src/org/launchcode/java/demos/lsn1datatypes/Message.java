5e1af937c48ce50342982718e3f74af7a07cd0ad
lburk87 <lburk87@gmail.com>
2020-08-12 09:48:24 -0500

    2.2, 2.3

--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("dg")) {
+            return "Woof, Woof!";
         } else {
             return "Hello, World!";
         }
