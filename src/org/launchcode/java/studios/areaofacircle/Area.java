96ab3f73fcdaa4efadf03457be108a0a928dae9f
lburk87 <lburk87@gmail.com>
2020-08-13 08:23:18 -0500

    added validation with if statement and while loop

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -8,7 +8,15 @@ public class Area {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
+        while (!input.hasNextDouble()) {
+            System.out.println("Invalid. Enter a radius:");
+            input.next();
+        }
         double r = input.nextDouble();
+        if (r < 0) {
+            System.out.println("Invalid. Enter a radius:");
+            r = input.nextDouble();
+        }
         double area = (double) (Math.round(Circle.getArea(r) * 1000))/1000;
         System.out.println("The area of a circle of radius " + r + " is: " + area);
 
