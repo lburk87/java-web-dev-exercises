ecaa5003377c84d72c603ef09b19d58c5a14b8e1
lburk87 <lburk87@gmail.com>
2020-08-13 07:54:04 -0500

    2.5 studio, added Circle class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,16 @@
 package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
 
 public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius:");
+        double r = input.nextDouble();
+        double area = (double) (Math.round(Circle.getArea(r) * 1000))/1000;
+        System.out.println("The area of a circle of radius " + r + " is: " + area);
+
+    }
 }
