568a0659cbee7aff285a05f28f9322f2a4170eaa
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-08-31 21:28:06 -0500

    6.2 JUnit try it

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,9 +1,39 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
+import org.junit.Test;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
+
+import org.junit.Before;
+
 
 public class CarTest {
+    Car test_car;
+
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+//    @Test
+//    public void emptyTest() {
+//        assertEquals(10,10,.001);
+//    }
     //TODO: constructor sets gasTankLevel properly
+//    @Test
+//    public void testInitialGasTank() {
+//        assertEquals(10, test_car.getGasTankLevel(), .001);
+////    }
+//    @Test
+//    public void testInitialGasTank() {
+//        assertFalse(test_car.getGasTankLevel() == 0);
+//    }
+    @Test
+    public void testInitialGasTank() {
+        assertTrue(test_car.getGasTankLevel() == 10);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
